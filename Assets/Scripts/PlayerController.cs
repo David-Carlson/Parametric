4eccc6c13f1765bee908feb057cf0f438e8b6d8b
David-Carlson <britishorbust@gmail.com>
2015-04-16 10:25:12 -0400

    Have base game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private float speed = 10f;
+    public EnemyPlayer enemy;
+    void FixedUpdate()
+    {
+        void Start()
+        {
+            
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            dir.Normalize();
+            rigidbody2D.velocity = dir*speed;
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Vector2 normal = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            normal.Normalize();
+
+            rigidbody2D.velocity *= -1;
+
+        }
+
+        Vector2 force = new Vector2(Input.GetAxis("Horizontal"), 0);
+        rigidbody2D.AddForce(force * speed);
+
+    }
+}
---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPhysics : MonoBehaviour
+{
+
+    private SphereCollider sphereCollider;
+
+    private Ray ray;
+    //RayCastHit
+
+}
