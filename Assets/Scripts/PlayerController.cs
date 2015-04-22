a3d5b5c753e5036f941ba7895f8baa442d0aee6f
David-Carlson <britishorbust@gmail.com>
2015-04-22 18:36:53 -0400

    Changed to unity 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,19 +30,19 @@ public class PlayerController : MonoBehaviour
         {
             Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
             dir.Normalize();
-            rigidbody2D.velocity = dir*speed;
+            GetComponent<Rigidbody2D>().velocity = dir*speed;
         }
         if (Input.GetButtonDown("Fire2"))
         {
             Vector2 normal = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
             normal.Normalize();
 
-            rigidbody2D.velocity *= -1;
+            GetComponent<Rigidbody2D>().velocity *= -1;
 
         }
 
         Vector2 force = new Vector2(Input.GetAxis("Horizontal"), 0);
-        rigidbody2D.AddForce(force * speed);
+        GetComponent<Rigidbody2D>().AddForce(force * speed);
 
     }
 }
