aed060d644bb37393f370f7327c6be417ce76991
David-Carlson <britishorbust@gmail.com>
2015-04-28 00:54:29 -0400

    Added jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
     
-    private float speed = 10f;
+    public float speed = 10f;
+    public float JumpSpeed = 10f;
+
     private Rigidbody2D physicsSphere;
-    private bool OnGround = false;
+
+    public Transform groundCheck;
+    public float radius;
+    public bool Grounded = false;
+    public LayerMask groundLayer;
+
+    public int MaxTeleports = 3;
+    public float AnalogueResetAmt = 0.5f;
+    private bool AnalogueReset = true;
 
 
     private enum BallState
@@ -27,8 +39,7 @@ public class PlayerController : MonoBehaviour
 
     // First is used to remember the last direction fully pressed down
     // Used for charging shots, doing drop shots
-    public Vector2 firstAnalogueDir = new Vector2();
-    public Vector2 secondAnalogueDir = new Vector2();
+    public List<Vector2> analogueDirRequests = new List<Vector2>();
 
     void OnCollisionEnter2D(Collision2D coll)
     {
@@ -40,6 +51,13 @@ public class PlayerController : MonoBehaviour
         physicsSphere = GetComponent<Rigidbody2D>();
     }
 
+
+
+    void FixedUpdate()
+    {
+        Grounded = Physics2D.OverlapCircle(groundCheck.position, radius, groundLayer);
+    }
+
     // These handle state changes
     void Update()
     {
@@ -48,10 +66,9 @@ public class PlayerController : MonoBehaviour
             // When the player is holding B in the air to charge the boost
             // Cancels if hitting the ground before letting go, 
             case BallState.BoostCharging:
-                if (OnGround)
+                if (Grounded)
                 {
                     state = BallState.Stunned;
-
                 }
                     
                 break;
@@ -60,11 +77,10 @@ public class PlayerController : MonoBehaviour
 
                 break;
             case BallState.Dropping:
-                if(this.g)
+                
                 break;
 
             case BallState.DropStick:
-
                 break;
 
             case BallState.TeleportCharging:
@@ -73,8 +89,79 @@ public class PlayerController : MonoBehaviour
             case BallState.Teleporting:
 
                 break;
+            case BallState.None:
+                
+                HandleHorizontalMovement(speed);
+                if (Grounded)
+                {
+                    if (Input.GetButtonDown("Down Move"))
+                    {
+                        physicsSphere.velocity = new Vector2(physicsSphere.velocity.x, JumpSpeed);
+                        break;
+                    }
+
+                    if (Input.GetButton("Right Move"))
+                    {
+                        Console.WriteLine("Right boost");
+                        HandleHorizontalMovement(speed * 2);
+                    }
+
+                    if (Input.GetButtonDown("Up Move"))
+                    {
+                        Console.WriteLine("Tele move");
+                        state = BallState.TeleportCharging;
+                        analogueDirRequests.Clear();
+                        SetLastAnalogueDir();
+                    }
+                }
+                else
+                {
+                    if (Input.GetButtonDown("Down Move"))
+                    {
+                        Console.WriteLine("Attempng jump in air");
+                        state = BallState.Dropping;
+                        break;
+                    }
+
+                    if (Input.GetButtonDown("Right Move"))
+                    {
+                        state = BallState.BoostCharging;
+                        break;
+                    }
+
+                    if (Input.GetButtonDown("Up Move"))
+                    {
+                        state = BallState.TeleportCharging;
+                        analogueDirRequests.Clear();
+                        SetLastAnalogueDir();
+                    }
+                    
+                }
+                break;
         }
        
+    }
+
+    void SetLastAnalogueDir()
+    {
+        Vector2 currDir = GetAnalogueInput();
+        if (AnalogueReset == false && currDir.SqrMagnitude() <= 0.9f)
+            AnalogueReset = true;
+
+        if(AnalogueReset == true)
+        if (state == BallState.TeleportCharging)
+        {
+            currDir.Normalize();
+            analogueDirRequests.Add(currDir);
+            AnalogueReset = false;
+        }
+        else
+        {
+            currDir.Normalize();
+            analogueDirRequests.Clear();
+            analogueDirRequests.Add(currDir);
+            AnalogueReset = false;
+        }
     }
 
     Vector2 GetAnalogueInput()
@@ -82,9 +169,9 @@ public class PlayerController : MonoBehaviour
         return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
-    void HandleHorizontalMovement()
+    void HandleHorizontalMovement(float magnitude)
     {
-        physicsSphere.AddForce(new Vector2(Input.GetAxis("Horizontal")*speed, 0));
+        physicsSphere.AddForce(new Vector2(Input.GetAxis("Horizontal")*magnitude, 0));
     }
 
     IEnumerator StunCoroutine(float stunTime)
@@ -97,14 +184,5 @@ public class PlayerController : MonoBehaviour
 
 
         yield return null;
-    }
-
-  
-
-    void FixedUpdate()
-    {
-        //OnGround = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer).
-
-
     }
 }
